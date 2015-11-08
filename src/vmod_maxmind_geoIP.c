bad782e712b27afd8b50b04140a567eff88f7458
Vincent ROBERT <v.robert@of2m.fr>
2015-11-08 12:37:24 +0100

    initial

--- src/vmod_maxmind_geoIP.c
+++ src/vmod_maxmind_geoIP.c
@@ -1,11 +1,21 @@
+#define HOSTNAME_MAX_SIZE 200
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 
 #include "vrt.h"
+#include "vrt_obj.h"
 #include "cache/cache.h"
 
 #include "vcc_if.h"
+
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <GeoIP.h>
+
+static GeoIP *gi = NULL;
+static GeoIP *giV6 = NULL;
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
@@ -14,12 +24,37 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 }
 
 VCL_VOID
-vmod_set_x_request_start(const struct vrt_ctx *ctx)
+vmod_get_country_code(const struct vrt_ctx *ctx)
 {
-  struct timeval detail_time;
-  gettimeofday(&detail_time,NULL);
-  char start[20];
-  sprintf(start, "t=%lu%06lu", detail_time.tv_sec, detail_time.tv_usec);
-  static const struct gethdr_s VGC_HDR_REQ_VARNISH_X_REQ_START = { HDR_REQ, "\020X-Request-Start:"};
-  VRT_SetHdr(ctx, &VGC_HDR_REQ_VARNISH_X_REQ_START, start, vrt_magic_string_end);
+  vmod_get_country_code_IP(ctx, VRT_r_client_ip(ctx));
+}
+
+VCL_VOID
+vmod_get_country_code_IP(const struct vrt_ctx *ctx, VCL_IP client_ip)
+{
+  const char* country = NULL;
+
+  if (gi == NULL)
+    gi = GeoIP_open_type(GEOIP_COUNTRY_EDITION , GEOIP_STANDARD);
+
+  if (giV6 == NULL)
+    giV6 = GeoIP_open_type(GEOIP_COUNTRY_EDITION_V6 , GEOIP_STANDARD);
+
+  const char* ip=VRT_IP_string(ctx, client_ip);
+  //const char* ip="6.6.6.6";
+
+  if (ip != NULL) {
+    if (strstr(ip, ":") != NULL) {
+      country = GeoIP_country_code_by_addr_v6(giV6, ip);
+    }
+    else {
+      country = GeoIP_country_code_by_addr(gi, ip);
+    }
+  }
+
+
+  country = country ? country : "Unknown";
+
+  static const struct gethdr_s VGC_HDR_REQ_VARNISH_X_COUNTRY_CODE = { HDR_REQ, "\017X-Country-Code:"};
+  VRT_SetHdr(ctx, &VGC_HDR_REQ_VARNISH_X_COUNTRY_CODE, country, vrt_magic_string_end);
 }
